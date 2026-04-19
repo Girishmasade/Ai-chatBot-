a504652a5f7696d4da79a972ad96ca126c874792
Girishmasade <girishmasade@gmail.com>
2026-04-19 22:42:56 +0530

    feat: integrate Redis client for server-side caching and state management
    Signed-off-by: Girishmasade <girishmasade@gmail.com>

--- server/src/env/env.import.ts
+++ server/src/env/env.import.ts
@@ -2,5 +2,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const PORT = process.env.PORT || 5000 as unknown as number
+const redisUrl = process.env.REDIS_URL as string
 
-
+export {redisUrl}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import { app, server } from "./socket/socket.js";
 import { PORT } from "./env/env.import.js";
+import redisClient from "./config/redis.config.js";
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+redisClient.connect()
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
